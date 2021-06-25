8c1fb300ef5fae4b369e5c73bcdee045c01c240e
Audrey Xie <audrey.hy.xie@gmail.com>
2021-06-25 16:18:06 -0400

    Third graph for solve time

--- mpcc/mpcc_animation.py
+++ mpcc/mpcc_animation.py
@@ -1,6 +1,7 @@
 from mpcc_solver import build_solver
 from mpcc_utils import gen_t, compute_step
 
+import time
 import casadi as cd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -16,16 +17,16 @@ e = 0.07
 keep_going = True
 num_targets = 0
 
-fig, (ax1, ax2) =  plt.subplots(1, 2, figsize=(10, 5))
+fig, (ax1, ax2, ax3) =  plt.subplots(1, 3, figsize=(15, 5))
 
-# 5th-order
-xs, ys = 0, 0
-xt, yt = 3.3, 2
-        # [x, y, phi, delta, vx, theta]
-init_ts = [xs, ys, cd.pi/3, 0, 0, 0]
-xpts = [xs] + [.5, 2] + [xt]
-ypts = [ys] + [1, 3] + [yt]
-order = 5
+# # 5th-order
+# xs, ys = 0, 0
+# xt, yt = 3.3, 2
+#         # [x, y, phi, delta, vx, theta]
+# init_ts = [xs, ys, cd.pi/3, 0, 0, 0]
+# xpts = [xs] + [.5, 2] + [xt]
+# ypts = [ys] + [1, 3] + [yt]
+# order = 5
 
 # # 5th-order
 # xs, ys = 0, 0
@@ -45,14 +46,14 @@ order = 5
 # ypts = [ys] + [1, 2] + [yt]
 # order = 3
 
-# # 3rd-order
-# xs, ys = -0.3, 0
-# xt, yt = 2, 3
-#         # [x, y, phi, delta, vx, theta]
-# init_ts = [xs, ys, 2*cd.pi/3, 0, 0, 0]
-# xpts = [xs] + [0, 1] + [xt]
-# ypts = [ys] + [1.5, 1.75] + [yt]
-# order = 3
+# 3rd-order
+xs, ys = -0.3, 0
+xt, yt = 2, 3
+        # [x, y, phi, delta, vx, theta]
+init_ts = [xs, ys, 2*cd.pi/3, 0, 0, 0]
+xpts = [xs] + [0, 1] + [xt]
+ypts = [ys] + [1.5, 1.75] + [yt]
+order = 3
 
 # # 1st-order
 # xs, ys = 0, 0
@@ -73,8 +74,11 @@ print(cx, cy)
 
 solver, params = build_solver(init_ts, T, N, D, order, xpoly, ypoly)
 
+time_y = [None]
+time_x = [0]
+
 def solve_mpcc(w0_tmp=None):
-    global w_opt
+    global w_opt, time_y, time_x
     w0, lbw, ubw, lbg, ubg = params
 
     if w0_tmp == None:
@@ -83,11 +87,14 @@ def solve_mpcc(w0_tmp=None):
     lbw = init_ts + lbw
     ubw = init_ts + ubw
 
+    time_before_sol = time.time()
     sol = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg, p=cd.vertcat(xt, yt, cx, cy))
-    cost = sol['f'].full().flatten()
-    # print(cost)
+    time_after_sol = time.time()
+    diff_sol = time_after_sol - time_before_sol
+    time_y.append(diff_sol) # diff_sol
+    time_x.append(time_x[-1]+1)
+    # cost = sol['f'].full().flatten()
     w_opt = sol['x'].full().flatten()
-    # print(w_opt)
 
     return w_opt
 
@@ -126,7 +133,7 @@ def gen():
 def update(i):
     global x_line, y_line, aux_line, alphaux_line
     global curr_pt, target_pt, traj
-    global init_ts, xt, yt, num_targets, keep_going
+    global init_ts, xt, yt, num_targets, keep_going, time_x, time_y
     global x_opt, y_opt, psi_opt, delta_opt, vx_opt, theta_opt, alphaux_opt, aux_opt, dt_opt
 
     x_opt, y_opt, psi_opt, delta_opt, vx_opt, theta_opt, alphaux_opt, aux_opt, dt_opt = solved_vals()
@@ -143,12 +150,11 @@ def update(i):
     traj.set_data(x_opt, y_opt)
     curr_pt.set_data([x_opt[0]], [y_opt[0]])
 
+    time_pts.set_data(time_x, time_y)
+
     init = [x_opt[0], y_opt[0], psi_opt[0], delta_opt[0], vx_opt[0], theta_opt[0], alphaux_opt[0], aux_opt[0], dt_opt[0]]
 
     init_ts = compute_step(init, ts, D)
-    # print('\n\n')
-    # print(init_ts)
-    # print('\n\n')
 
     if abs(init_ts[0]-xt) < e and abs(init_ts[1]-yt) < e:
         keep_going = False
@@ -158,7 +164,7 @@ def update(i):
 def init_plot():
     global t_grid
     global x_line, y_line, aux_line, alphaux_line, curr_pt, target_pt, traj
-    global init_ts, xt, yt, num_targets, keep_going
+    global init_ts, xt, yt, num_targets, keep_going, time_pts
     global x_opt, y_opt, psi_opt, delta_opt, vx_opt, theta_opt, alphaux_opt, aux_opt, dt_opt
 
     tgrid = [T/N*k for k in range(N+1)]
@@ -194,20 +200,15 @@ def init_plot():
     target_pt, = ax2.plot([xt], [yt], marker='x', color='blue')
     ax2.grid()
 
+    ax3.set_xlim([0, 200])
+    ax3.set_ylim([0, .25])
+    time_pts, = ax3.plot(time_x, time_y, 'o', color='blue', lw=.2, alpha=0.3)
+    ax3.grid(axis='y')
+
 init_plot()
-# TODO: delete this print
-# print('\n\n w0')
-# nine_elem = []
-# for i, v in enumerate(w_opt):
-#     if (i+1) % 9 == 0:
-#         print(nine_elem + [v])
-#         nine_elem = []
-#     else: nine_elem.append(v)
-# print(nine_elem)
-# print('\n\n')
 
 writergif = animation.PillowWriter(fps=30)
 anim = animation.FuncAnimation(fig, update, interval=100, frames=gen, save_count=3000)
-anim.save('test_mpcc_1.gif', writer=writergif)
+anim.save('test_mpcc.gif', writer=writergif)
 
 # plt.show()
