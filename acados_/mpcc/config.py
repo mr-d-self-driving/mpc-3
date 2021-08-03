47829e74e5fdb413154580b2b8d047c88de3acab
Audrey Xie <audrey.hy.xie@gmail.com>
2021-08-03 10:04:30 -0400

    MPC acados generated code

--- acados_/mpcc/config.py
+++ acados_/mpcc/config.py
@@ -14,10 +14,10 @@ init_ts = np.array([0.0, 0.0, np.pi/4, 0.0, 0.0])
 
 curr_path = os.path.dirname(os.path.dirname(__file__)) # sorta hacky
 out_path = os.path.join(curr_path, 'out')
-code_export_dir = os.path.join(curr_path, 'mpc_acados_generated_code')
-json_path = 'mpc_acados_ocp.json'
+code_export_dir = os.path.join(curr_path, 'mpcc_acados_generated_code')
+json_path = 'mpcc_acados_ocp.json'
 
 os.makedirs(out_path, exist_ok=True)
 os.makedirs(code_export_dir, exist_ok=True)
 
-anim_save_file = os.path.join(out_path, 'acados_mpc.gif')
+anim_save_file = os.path.join(out_path, 'acados_mpcc.gif')
